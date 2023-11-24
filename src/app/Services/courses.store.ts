f160031024c9574cc4b0243e952b503c7c4862c0
Maxime MÉDAL <medal.max@gmail.com>
2023-11-24 16:54:59 +0100

    WIP

--- src/app/Services/courses.store.ts
+++ src/app/Services/courses.store.ts
@@ -1,13 +1,43 @@
 import { Injectable } from "@angular/core";
-import { Observable } from "rxjs";
+import { BehaviorSubject, Observable, throwError } from "rxjs";
 import { Course, sortCoursesBySeqNo } from "../model/course";
-import { map } from "rxjs/operators";
+import { catchError, map } from "rxjs/operators";
+import { HttpClient } from "@angular/common/http";
+import { LoadingService } from "../loading/loading.service";
+import { MessagesService } from "../messages/messages.service";
 
 @Injectable({
     providedIn: 'root'
 })
 export class CoursesStore {
-    public courses$: Observable<Course[]>;
+    private apiUrl = "/api/course";
+    private subject = new BehaviorSubject<Course[]>([]);
+
+    public courses$: Observable<Course[]> = this.subject.asObservable();
+
+
+    constructor(
+        private http: HttpClient,
+        private loadingService: LoadingService,
+        private messageService: MessagesService
+    ) {
+        this.loadAllCourses();
+    }
+
+    private loadAllCourses() {
+        const loadCourses$ = this.http.get<Course[]>(this.apiUrl)
+            .pipe(
+                map(res => res["payload"]),
+                catchError(err => {
+                    const message = "Could not load courses";
+                    console.error(message, err)
+                    this.messageService.showErrors(message);
+                    return throwError(err);
+                })
+            )
+
+        this.loadingService.showLoaderUntilCompleted(loadCourses$).subscribe();
+    }
 
     filterByCategorie(category: string): Observable<Course[]> {
         return this.courses$
